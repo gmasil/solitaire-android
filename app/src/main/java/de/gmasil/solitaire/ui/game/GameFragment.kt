967dc308708a147c4c7ab3bdca5b89c4b7f2e617
gmasil <54176035+gmasil@users.noreply.github.com>
2024-05-12 18:14:22 +0200

    implement snapback animation

--- app/src/main/java/de/gmasil/solitaire/ui/game/GameFragment.kt
+++ app/src/main/java/de/gmasil/solitaire/ui/game/GameFragment.kt
@@ -7,13 +7,13 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.TranslateAnimation
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import de.gmasil.solitaire.R
 import de.gmasil.solitaire.databinding.FragmentGameBinding
-
 
 class GameFragment : Fragment() {
 
@@ -24,7 +24,9 @@ class GameFragment : Fragment() {
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -32,45 +34,56 @@ class GameFragment : Fragment() {
         val textView: TextView = binding.textGame
         textView.text = requireContext().getString(R.string.menu_game)
 
-        val gameField = root.findViewById<RelativeLayout>(R.id.game_field)
+        val gameFieldView = root.findViewById<RelativeLayout>(R.id.game_field)
 
         imageView = ImageView(requireContext())
-        imageView!!.setImageResource(R.drawable.ace_of_spades);
+        imageView!!.setImageResource(R.drawable.ace_of_spades)
         val layoutParams = RelativeLayout.LayoutParams(cw, ch)
         imageView!!.setLayoutParams(layoutParams)
         imageView!!.setBackgroundResource(R.drawable.border)
         imageView!!.x = cx
         imageView!!.y = cy
 
-        gameField.addView(imageView)
+        gameFieldView.addView(imageView)
 
-        gameField.setOnTouchListener { view, event ->
+        gameFieldView.setOnTouchListener { view, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     startDragging(event.x, event.y)
                 }
-
                 MotionEvent.ACTION_UP -> {
-                    Log.i("touch", "stop dragging")
+                    if (dragging) {
+                        Log.i("touch", "stop dragging")
+                        snapCardTo(imageView!!, 500f, 0f)
+                    }
                     dragging = false
                 }
-
                 MotionEvent.ACTION_MOVE -> {
                     if (dragging) {
                         updateCard(event.x, event.y)
                     }
                 }
-
                 else -> {
                     Log.i(
-                        "touch", "got ${event.action} ${MotionEvent.actionToString(event.action)}"
-                    )
+                        "touch", "got ${event.action} ${MotionEvent.actionToString(event.action)}")
                 }
             }
             true
         }
 
         return root
+    }
+
+    private fun snapCardTo(view: View, x: Float, y: Float) {
+        val animation = TranslateAnimation(0f, x - cx, 0f, y - cy)
+        animation.setDuration(100)
+        animation.fillAfter = false
+        animation.setAnimationListener(
+            SnapAnimationListener(view, x, y) { x, y ->
+                cx = x
+                cy = y
+            })
+        view.startAnimation(animation)
     }
 
     private fun updateCard(x: Float, y: Float) {
---
+++ app/src/main/java/de/gmasil/solitaire/ui/game/SnapAnimationListener.kt
@@ -0,0 +1,24 @@
+package de.gmasil.solitaire.ui.game
+
+import android.view.View
+import android.view.animation.Animation
+import java.util.function.BiConsumer
+
+class SnapAnimationListener(
+    private val view: View,
+    private val x: Float,
+    private val y: Float,
+    private val callback: BiConsumer<Float, Float>
+) : Animation.AnimationListener {
+
+    override fun onAnimationStart(animation: Animation?) {}
+
+    override fun onAnimationEnd(animation: Animation?) {
+        view.clearAnimation()
+        view.x = x
+        view.y = y
+        callback.accept(x, y)
+    }
+
+    override fun onAnimationRepeat(animation: Animation?) {}
+}
