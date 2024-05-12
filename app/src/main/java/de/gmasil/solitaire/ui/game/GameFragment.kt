7d87ead4cc26f49180909a88b103715520ef69e8
gmasil <54176035+gmasil@users.noreply.github.com>
2024-05-12 14:02:21 +0200

    implement basic drag and drop of card

--- app/src/main/java/de/gmasil/solitaire/ui/game/GameFragment.kt
+++ app/src/main/java/de/gmasil/solitaire/ui/game/GameFragment.kt
@@ -7,10 +7,13 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import de.gmasil.solitaire.R
 import de.gmasil.solitaire.databinding.FragmentGameBinding
+
 
 class GameFragment : Fragment() {
 
@@ -21,9 +24,7 @@ class GameFragment : Fragment() {
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -31,13 +32,39 @@ class GameFragment : Fragment() {
         val textView: TextView = binding.textGame
         textView.text = requireContext().getString(R.string.menu_game)
 
-        root.setOnTouchListener { view, event ->
+        val gameField = root.findViewById<RelativeLayout>(R.id.game_field)
+
+        imageView = ImageView(requireContext())
+        imageView!!.setImageResource(R.drawable.ace_of_spades);
+        val layoutParams = RelativeLayout.LayoutParams(cw, ch)
+        imageView!!.setLayoutParams(layoutParams)
+        imageView!!.setBackgroundResource(R.drawable.border)
+        imageView!!.x = cx
+        imageView!!.y = cy
+
+        gameField.addView(imageView)
+
+        gameField.setOnTouchListener { view, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    Log.i("touch", "action down!!")
+                    startDragging(event.x, event.y)
                 }
+
+                MotionEvent.ACTION_UP -> {
+                    Log.i("touch", "stop dragging")
+                    dragging = false
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    if (dragging) {
+                        updateCard(event.x, event.y)
+                    }
+                }
+
                 else -> {
-                    Log.i("touch", "got ${event.action}")
+                    Log.i(
+                        "touch", "got ${event.action} ${MotionEvent.actionToString(event.action)}"
+                    )
                 }
             }
             true
@@ -45,6 +72,42 @@ class GameFragment : Fragment() {
 
         return root
     }
+
+    private fun updateCard(x: Float, y: Float) {
+        cx = x - dx
+        cy = y - dy
+        imageView!!.x = cx
+        imageView!!.y = cy
+    }
+
+    private fun isTouchingCard(x: Float, y: Float): Boolean {
+        return x >= cx && x <= cx + cw && y >= cy && y <= cy + ch
+    }
+
+    private fun startDragging(x: Float, y: Float) {
+        if (!isTouchingCard(x, y)) {
+            dragging = false
+            return
+        }
+        dragging = true
+        dx = x - cx
+        dy = y - cy
+        Log.i("touch", "start dragging $dx, $dy")
+        updateCard(x, y)
+    }
+
+    var dragging = false
+    var imageView: ImageView? = null
+
+    // card position and size
+    var cx: Float = 20.0f
+    var cy: Float = 20.0f
+    var cw: Int = 207
+    var ch: Int = 300
+
+    // delta x,y from dragging start to card position
+    var dx: Float = 0.0f
+    var dy: Float = 0.0f
 
     override fun onDestroyView() {
         super.onDestroyView()
